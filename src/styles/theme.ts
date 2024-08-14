dbe02ffc845c010ea3553c117c955c4d5a43f047
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-14 09:12:13 -0300

    style: global themes

---
+++ src/styles/theme.ts
@@ -0,0 +1,62 @@
+const theme = {
+  colors: {
+    primary: {
+      main: "#001c98",
+      dark: "#796ce0",
+      medium: "#a594ff",
+      light: "#a594ff",
+    },
+    secondary: {
+      main: "#ffc14a",
+    },
+    grays: {
+      gray50: "#fafafa",
+      gray100: "#f5f5f5",
+      gray200: "#eeeeee",
+      gray300: "#e0e0e0",
+      gray400: "#bdbdbd",
+      gray500: "#9e9e9e",
+      gray600: "#757575",
+      gray700: "#616161",
+      gray800: "#424242",
+      gray900: "#212121",
+    },
+    others: {
+      white: "#ffffff",
+      black: "#000000",
+    },
+  },
+  fonts: {
+    fontFamily: {
+      primary: "Sora, sans-serif",
+      secondary: "Poppins, sans-serif",
+    },
+    fontSizes: {
+      small: "0.8em",
+      normal: "1em",
+      large: "1.25em",
+      heading4: "2em",
+      heading3: "2.25em",
+    },
+    fontWeight: {
+      semiBold: "600",
+      medium: "400",
+    },
+  },
+  borders: {
+    shadows: {
+      light: "0px 7px 30px 0px rgba(187, 192, 205, 0.4)",
+      dark: "0px 0px 10px 0px rgba(97, 97, 97, 0.3)",
+    },
+  },
+  statusAlert: {
+    success: "#00c247",
+    info: "#004ce8",
+    infoMedium: "#87aeff",
+    infoLight: "#def1ff",
+    alert: "#ffe16a",
+    error: "#ff3333",
+  },
+};
+
+export default theme;
