76d15596c8189d4a7e9f30d0776fb38c20f46979
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-14 09:05:00 -0300

    feat: implementing the global theme, along with the global reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/styleGlobal";
+import Router from "./routes/Routes";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
