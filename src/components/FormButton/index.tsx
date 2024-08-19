d63887a1cf012f662276c6b47a427a5620080b37
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-19 09:43:29 -0300

    feat/style: including the disabled + the styling of the disabled part

--- src/components/FormButton/index.tsx
+++ src/components/FormButton/index.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import { ButtonProps } from "../../types/typesComponents";
 import { Button } from "./style";
 
-const FormButton: React.FC<ButtonProps> = ({ children, onClick }) => {
+const FormButton: React.FC<ButtonProps> = ({ children, onClick, disabled }) => {
   return (
     <>
-      <Button onClick={onClick}>{children}</Button>
+      <Button onClick={onClick} disabled={disabled}>
+        {children}
+      </Button>
     </>
   );
 };
--- src/components/FormButton/style.ts
+++ src/components/FormButton/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 import theme from "../../styles/theme";
 
-export const Button = styled.button`
+export const Button = styled.button<{ disabled?: boolean }>`
   width: 100%;
   padding: 14px;
   border: none;
@@ -10,12 +10,14 @@ export const Button = styled.button`
   font-size: ${theme.fonts.fontSizes.normal};
   font-family: ${theme.fonts.fontFamily.secondary};
   font-weight: ${theme.fonts.fontWeight.semiBold};
-  cursor: pointer;
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   color: ${theme.colors.others.white};
-  background-color: ${theme.colors.secondary.main};
+  background-color: ${({ disabled }) =>
+    disabled ? theme.colors.grays.gray400 : theme.colors.secondary.main};
   transition: 0.2s;
   &:hover {
-    background-color: ${theme.colors.primary.main};
+    background-color: ${({ disabled }) =>
+      disabled ? theme.colors.grays.gray400 : theme.colors.primary.main};
     transition: 0.2s;
   }
 `;
