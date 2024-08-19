1ccdbd1cbc41b4e0b541380e5495e564fe43bd5d
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-19 09:44:14 -0300

    feat/style: including alert colors in inputs + stylization of the disabled part

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
 
-import { FormInputProps } from "../../types/typesComponents";
 import OpenEye from "../../assets/imgSGV/OpenEye";
 import ClosedEye from "../../assets/imgSGV/ClosedEye";
 import { ButtonEye, ContainerInput, Label } from "./style";
+import { FormInputProps } from "../../types/typesComponents";
 
 const FormInput: React.FC<FormInputProps> = ({
   label,
   placeholder,
   htmlFor,
   isPassword,
-  onChange,
   value,
+  onChange,
+  colorAlert,
 }) => {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -21,24 +22,20 @@ const FormInput: React.FC<FormInputProps> = ({
 
   return (
     <>
-      {isPassword ? (
-        <>
-          <Label htmlFor={htmlFor}>{label}</Label>
-          <ContainerInput
-            type={showPassword ? "text" : "password"}
-            placeholder={placeholder}
-            value={value}
-            onChange={onChange}
-          />
-          <ButtonEye onClick={eyeTogglePassword}>
-            {showPassword ? <OpenEye /> : <ClosedEye />}
-          </ButtonEye>
-        </>
-      ) : (
-        <>
-          <Label htmlFor={htmlFor}>{label}</Label>
-          <ContainerInput type="text" placeholder={placeholder} />
-        </>
+      <Label htmlFor={htmlFor} colorAlert={colorAlert}>
+        {label}
+      </Label>
+      <ContainerInput
+        type={isPassword && !showPassword ? "password" : "text"}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        colorAlert={colorAlert}
+      />
+      {isPassword && (
+        <ButtonEye onClick={eyeTogglePassword}>
+          {showPassword ? <OpenEye /> : <ClosedEye />}
+        </ButtonEye>
       )}
     </>
   );
--- src/components/FormInput/style.ts
+++ src/components/FormInput/style.ts
@@ -2,27 +2,36 @@ import styled from "styled-components";
 
 import theme from "../../styles/theme";
 
-export const Label = styled.label`
+export const Label = styled.label<{ colorAlert?: string }>`
   padding: 04px;
   font-family: ${theme.fonts.fontFamily.secondary};
   font-size: ${theme.fonts.fontSizes.small};
   position: relative;
   top: 10px;
   left: 20px;
-  color: ${theme.colors.grays.gray600};
+  color: ${({ colorAlert }) => colorAlert};
   background-color: ${theme.colors.others.white};
 `;
-export const ContainerInput = styled.input`
+export const ContainerInput = styled.input<{ colorAlert?: string }>`
   width: 92%;
   height: 37px;
   padding: 05px 15px;
-  border: 01px solid ${theme.colors.grays.gray400};
+  border: 01px solid ${({ colorAlert }) => colorAlert};
   border-radius: 15px;
+  &:focus {
+    outline: none;
+  }
 `;
 export const ButtonEye = styled.span`
+  width: 25px;
+  height: 25px;
   border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: relative;
-  bottom: 36px;
-  left: 355px;
+  bottom: 35px;
+  left: 87%;
+  cursor: pointer;
   background-color: ${theme.colors.others.white};
 `;
