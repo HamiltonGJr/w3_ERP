a9affbc3ca6ed8833f3b5fc52dff16664850353c
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-29 14:10:29 -0300

    feat: typing of MenuTableProps, TableProps, SearchHeaderProps, CardPredictionProps components

--- src/types/typesComponents.ts
+++ src/types/typesComponents.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, ReactElement, ReactNode } from "react";
 
 export interface FormInputProps {
   label: string;
@@ -27,4 +27,31 @@ export interface GraphicCardProps {
   highOrLowPercentage: string;
   percentageGraphic: number;
   isPositive: boolean;
+}
+
+export interface MenuTableProps {
+  icon: ReactElement;
+  title: string;
+  buttonHighOrLow?: ReactElement;
+  children: ReactNode;
+  generalwidthContainer: string;
+  backgroundColor: string;
+}
+
+export interface TableProps {
+  headers: string[];
+  children: ReactNode;
+}
+
+export interface SearchHeaderProps {
+  title: string;
+  onClickSearch?: () => void;
+  iconFilter?: ReactElement;
+  onClickFilter?: () => void;
+}
+
+export interface CardPredictionProps {
+  client: string;
+  onClickNavigate?: () => void;
+  id: string;
 }
