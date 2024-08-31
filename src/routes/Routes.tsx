e4a6507e08d49ae3db02cb82768e147f761b5f39
Hamilton Gonçalves <143231573+HamiltonGJr@users.noreply.github.com>
2024-08-31 11:28:51 -0300

    feat: including other routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import Prediction from "../pages/Prediction";
+import Product from "../pages/Product";
+import DetailProduct from "../pages/DetailProduct";
+import DetailClient from "../pages/DetailClient";
+import DetailPrediction from "../pages/DetailPrediction";
 
 const Router = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Login />}></Route>
+          <Route index element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/prediction" element={<Prediction />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/products/:id" element={<DetailProduct />} />
+          <Route path="/customers/:id" element={<DetailClient />} />
+          <Route path="/prediction/:id" element={<DetailPrediction />} />
         </Routes>
       </BrowserRouter>
     </>
